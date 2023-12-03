fb7603783a6718d0f0ed989fe73f34554fe3a2cb
Tezeghdenti Rahma <rahmatezeghdenti@gmail.com>
2023-12-03 11:25:34 -0100

    init commit

---
+++ components/Student/student.tsx
@@ -0,0 +1,6 @@
+
+function Student () {
+    return <h1>Student</h1>
+}
+
+export default Student;
---
+++ components/Teacher/Teacher.tsx
@@ -0,0 +1,6 @@
+
+function Teacher () {
+    return <h1>Teacher</h1>
+}
+
+export default Teacher;
